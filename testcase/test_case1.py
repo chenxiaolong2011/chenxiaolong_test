d0e9ca5896cbbc33161ed7231ae28d605eb35612
chenxiaolong2011 <15828563390@163.com>
2024-11-17 19:48:43 +0800

    日志

--- testcase/test_case1.py
+++ testcase/test_case1.py
@@ -1,16 +1,20 @@
 
 import pytest
+import logging
 
 
 class Test_test1:
     @pytest.mark.smoking
     def test_case1(self):
+        logging.info('test_case1')
         assert 1 == 1
 
     @pytest.mark.smoking
     def test_case3(self):
+        logging.info('test_case3')
         assert 1 == 1
 
     @pytest.mark.other
     def test_case2(self):
+        logging.info('test_case2')
         assert 1 == 1
