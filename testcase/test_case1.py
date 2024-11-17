bb2da03773ae397a3bfdf7b627aa2a2eadab5176
chenxiaolong2011 <15828563390@163.com>
2024-11-17 16:55:37 +0800

    test

--- testcase/test_case1.py
+++ testcase/test_case1.py
@@ -7,6 +7,10 @@ class Test_test1:
     def test_case1(self):
         assert 1 == 1
 
+    @pytest.mark.smoking
+    def test_case3(self):
+        assert 1 == 1
+
     @pytest.mark.other
     def test_case2(self):
-        assert 1 == 2
+        assert 1 == 1
